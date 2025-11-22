c232714b40762fdf3f6e6505db1b3f807fedf7ba
Dhamodharan450 <damodharan@hyperready.tech>
2025-11-22 11:23:09 +0530

    Add files via upload

---
+++ src/tools/index.ts
@@ -0,0 +1,5 @@
+// export {  listTablesToolTool, listColumnsTool, saveKPITool } from './db_kpi_tools';
+export { fetchKPIsTool, runQueryTool,saveInsightTool } from './db_insight_tools';
+export { ensureTables, pool } from './db_shared';
+
+export { generateSQLTool } from './sql_generator';
